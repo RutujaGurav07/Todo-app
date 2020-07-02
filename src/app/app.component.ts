ac9b3bedabc3431c8c01d22015e867985c514f8a
Rutuja Gurav <rutujagurav72@gmail.com>
2020-07-02 10:54:22 +0530

    creating todo forn with add delete option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray = [];
+  todos;
+
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todos);
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+    console.log("delete item");
+  }
+
+  todoSubmit(value: any) {
+    // console.log(value);
+    if (value !== "") {
+      this.todoArray.push(value.todo)
+    }else{
+      alert("Field required")
+    }
+  }
 }
